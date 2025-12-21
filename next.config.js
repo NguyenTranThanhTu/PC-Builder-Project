e3fedfa7f3a0c549b9a1e0b03909a8cf6b7d53a1
NguyenTranThanhTu <thanhtu02102003@gmail.com>
2025-12-22 03:40:37 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'cdn2.cellphones.com.vn',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'tse1.mm.bing.net',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'res.cloudinary.com',
-				pathname: '/**',
-			},
-		],
-		domains: ['localhost', 'lh3.googleusercontent.com'],
-	},
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn2.cellphones.com.vn',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tse1.mm.bing.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+    domains: ['localhost', 'lh3.googleusercontent.com'],
+  },
 };
 
 module.exports = nextConfig;
